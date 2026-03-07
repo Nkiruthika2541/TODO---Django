a9dc5182edb84d075cf6b3fb37ace24fabcccd1d
Kiruthika <kiruthi2541@gmail.com>
2026-03-07 21:55:07 +0530

    Updated category and status views,delete page

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
   path('home/',views.home,name="home"),
   path('new_task/',views.create,name="create"),
   path('new_category/',views.category,name="category"),
+  path('view_by_category/<int:category_id>/',views.category_view,name="category_view"),
+  path('edit-category/',views.category_edit,name="category_edit"),
+  path('delete-category/',views.category_delete,name="category_delete"),
+  path('view_by_status/<str:status>/',views.status_view,name="status_view"),
   path('update/<int:task_id>/',views.update,name="update"),
   path('delete/<int:task_id>/',views.delete,name="delete"),
   
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from django.contrib import messages
 
@@ -12,9 +12,11 @@ def hello(request):
   
 def home(request):
   tasks = Todo_Task_Model.objects.all().order_by('-created_on')
-  return render(request,'HOME.html',{'tasks':tasks})
+  category = Category_Model.objects.all()
+  return render(request,'HOME.html',{'tasks':tasks,'category':category})
   
 def create(request):
+  category = Category_Model.objects.all()
   if request.method=="POST":
     form = Todo_Task_Form(request.POST)
     if form.is_valid():
@@ -23,10 +25,11 @@ def create(request):
       return redirect('home')
   else:
     form = Todo_Task_Form()
-    
-  return render(request,'Create.html',{"form":form})
+  
+  return render(request,'Create.html',{"form":form,'category':category})
   
 def category(request):
+  category = Category_Model.objects.all()
   if request.method == "POST":
     form = Category_Form(request.POST)
     if form.is_valid():
@@ -36,12 +39,12 @@ def category(request):
   else:
     form = Category_Form()
     
-  return render(request,'Category.html',{'form':form})
-  
+  return render(request,'Category.html',{'form':form,'category':category})
   
   
 def update(request,task_id):
-  update_task = Todo_Task_Model.objects.get(id=task_id)
+  category = Category_Model.objects.all()
+  update_task = get_object_or_404(Todo_Task_Model, id=task_id)
   if request.method=="POST":
     form = Update_Task_Form(request.POST,instance=update_task)
     if form.is_valid():
@@ -51,16 +54,39 @@ def update(request,task_id):
   else:
     form = Todo_Task_Form(instance=update_task)
     
-  return render(request,'Update.html',{"form":form})
+  return render(request,'Update.html',{"form":form,'category':category})
   
 def delete(request,task_id):
-  delete_task = Todo_Task_Model.objects.get(id=task_id)
+  category = Category_Model.objects.all()
+  delete_task = get_object_or_404(Todo_Task_Model, id=task_id)
   if request.method == 'POST':
       delete_task.delete()
       messages.success(request,'Task deleted Successfully')
       return redirect('home')
-  else:
-    messages.error(request,"Task doesn't exist.")
-  return render(request,'Delete.html',{'delete_task':det})
+  return render(request,'Delete.html',{'delete_task':delete_task,'category':category})
     
   
+def category_view(request,category_id):
+  category = Category_Model.objects.all()
+  tasks = Todo_Task_Model.objects.filter(category_id = category_id)
+  from_category = get_object_or_404(Category_Model, id=category_id)
+  return render(request,'By_Category.html',{'tasks':tasks,'category':category,"from_category":from_category})
+
+def status_view(request,status):
+  category = Category_Model.objects.all()
+  tasks = Todo_Task_Model.objects.filter(status = status)
+  from_status = status.capitalize()
+  print(from_status)
+  return render(request,'By_Status.html',{'tasks':tasks,'category':category,'from_status':from_status})
+
+def category_edit(request):
+  category = Category_Model.objects.all()
+  return render(request,'Edit_Category.html',{'category':category})
+  
+def category_delete(request):
+  category = Category_Model.objects.all()
+  if request.method == "POST":
+    checked_items = request.POST.getlist('category')
+    Category_Model.objects.filter(id__in = checked_items).delete()
+    return redirect('home')
+  return render(request,'Edit_Category.html',{'category':category})
